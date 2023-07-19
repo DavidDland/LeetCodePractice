b44cbb31298a3740fdbafc4161d30acb3d8703b9
DavidDland <120422159+DavidDland@users.noreply.github.com>
2023-07-19 15:32:46 -0400

    Time: 2 ms (91.76%), Space: 40.5 MB (83.89%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,31 +1,31 @@
 import java.util.Stack;
-
+//importing a stack to approach this so that I can pop values and check popped values against 
 class Solution {
     public boolean isValid(String s) {
 
         Stack<Character> stack = new Stack<>();
         char[] chars = s.toCharArray();
-
+//creating a char array to store and index more efficiently
         for (int i = 0; i < chars.length; i++) {
-            char newChar = chars[i];
+            char currChar = chars[i];
             if (chars[i] == '(' || chars[i] == '{' || chars[i] == '[') {
-                stack.push(chars[i]);
+                stack.push(chars[i]);   //popping opening brackets 
             } else {
-
+//test case if no opening brackets means no values get popped
                 if (stack.isEmpty()) {
                     return false;
                 }
-                Character popped = stack.pop();
+                char popped = stack.pop();
                 if (popped == '(') {
-                    if (newChar != ')') {
+                    if (currChar != ')') {
                         return false;
                     }
                 } else if (popped == '{') {
-                    if (newChar != '}') {
+                    if (currChar != '}') {
                         return false;
                     }
                 } else if (popped == '[') {
-                    if (newChar != ']') {
+                    if (currChar != ']') {
                         return false;
                     }
                 }
