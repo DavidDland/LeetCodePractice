2e913c9a0291b09a6c256333d1b6ca3c69c1c014
DavidDland <120422159+DavidDland@users.noreply.github.com>
2023-08-02 17:00:22 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (84.96%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,3 +1,4 @@
+//basically check if needle in haystick. Return index of needle in haystack. Return -1 if needle is not in haystack
 class Solution {
     public int strStr(String haystack, String needle) {
         if (haystack.contains(needle)) {
