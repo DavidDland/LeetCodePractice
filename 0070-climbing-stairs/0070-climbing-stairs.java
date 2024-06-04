f71346bea736ff1266b051ede8d80fe7adfd99f0
DavidDland <120422159+DavidDland@users.noreply.github.com>
2024-06-04 10:33:16 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (22.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+/**
+* Recursive and Dynamic programming
+*/ 
+public int climbStairs(int n) {
+    int memo[] = new int[n + 1];
+    return climb_Stairs(0, n, memo);
+}
+// Helper Function
+public int climb_Stairs(int i, int n, int memo[]) {
+    if (i > n) {
+        return 0;
+    }
+    if (i == n) {
+        return 1;
+    }
+    if (memo[i] > 0) {
+        return memo[i];
+    }
+
+    memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
+    return memo[i];
+    }
+}
